f779c60c91b1c8dd9de473f4ab48603abdfd9dd0
Роман Гринчак <gromrom22@gmail.com>
2026-03-10 10:35:29 +0200

    Add diagrams

--- ISTTP lab 1/Controllers/HomeController.cs
+++ ISTTP lab 1/Controllers/HomeController.cs
@@ -1,14 +1,59 @@
+using ISTTP_lab_1.Data;
 using ISTTP_lab_1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ISTTP_lab_1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vm = new DashboardViewModel();
+
+            vm.TotalGames = await _context.Games.CountAsync();
+            vm.TotalUsers = await _context.Users.CountAsync();
+            vm.TotalPcConfigs = await _context.PcConfigs.CountAsync();
+
+            var osData = await _context.PcConfigs
+                .GroupBy(p => p.Os)
+                .Select(g => new { OsName = g.Key.ToString(), Count = g.Count() })
+                .ToListAsync();
+
+            vm.OsLabels = osData.Select(x => x.OsName).ToList();
+            vm.OsCounts = osData.Select(x => x.Count).ToList();
+
+            var gpuData = await _context.PcConfigs
+                .Include(p => p.Gpu)
+                .GroupBy(p => p.Gpu.ModelName)
+                .Select(g => new { GpuName = g.Key, Count = g.Count() })
+                .OrderByDescending(x => x.Count)
+                .Take(5)
+                .ToListAsync();
+
+            vm.TopGpuLabels = gpuData.Select(x => x.GpuName).ToList();
+            vm.TopGpuCounts = gpuData.Select(x => x.Count).ToList();
+
+            var cpuData = await _context.PcConfigs
+                .Include(p => p.Cpu)
+                .GroupBy(p => p.Cpu.ModelName)
+                .Select(g => new { CpuName = g.Key, Count = g.Count() })
+                .OrderByDescending(x => x.Count)
+                .Take(5)
+                .ToListAsync();
+
+            vm.TopCpuLabels = cpuData.Select(x => x.CpuName).ToList();
+            vm.TopCpuCounts = cpuData.Select(x => x.Count).ToList();
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ ISTTP lab 1/Models/DashboardViewModelcs.cs
@@ -0,0 +1,15 @@
+﻿namespace ISTTP_lab_1.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalGames { get; set; }
+        public int TotalUsers { get; set; }
+        public int TotalPcConfigs { get; set; }
+        public List<string> OsLabels { get; set; } = new List<string>();
+        public List<int> OsCounts { get; set; } = new List<int>();
+        public List<string> TopGpuLabels { get; set; } = new List<string>();
+        public List<int> TopGpuCounts { get; set; } = new List<int>();
+        public List<string> TopCpuLabels { get; set; } = new List<string>();
+        public List<int> TopCpuCounts { get; set; } = new List<int>();
+    }
+}
